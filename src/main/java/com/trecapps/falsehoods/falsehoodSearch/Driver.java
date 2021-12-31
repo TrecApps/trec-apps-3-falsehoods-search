27f10317718908fcb7a419a2d7c0b03165c39731
John Jacko <jljacko@outlook.com>
2021-12-30 17:24:54 -0700

    enabled JPA repos added

--- src/main/java/com/trecapps/falsehoods/falsehoodSearch/Driver.java
+++ src/main/java/com/trecapps/falsehoods/falsehoodSearch/Driver.java
@@ -3,9 +3,11 @@ package com.trecapps.falsehoods.falsehoodSearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan({"com.trecapps.base.FalsehoodModel","com.trecapps.falsehoods.falsehoodSearch"})
+@EnableJpaRepositories(basePackages = {"com.trecapps.base.FalsehoodModel.repos", "com.trecapps.base.InfoResource.repos"})
 public class Driver {
 
     public static void main(String[] args) {
